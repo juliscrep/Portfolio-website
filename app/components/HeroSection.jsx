fa4a732ec0630c6749fb1d423b578e37c44cbde9
juliscrep <julietascrepnik@gmail.com>
2024-02-02 21:08:26 -0300

    se agrega about me section

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -30,8 +30,7 @@ const HeroSection = () => {
                        
                 </h1>
                 <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit voluptatibus. 
-                    Laboriosam placeat.
+                  Thank you for visiting. Welcome to my portfolio!
                 </p>
                 <div>
                     <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>Hire me</button>
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
     <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
         <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-2'> 
             <Link href={"/"} className='text-2xl md:text-5xl text-white font-semibold'>
-                LOGO
+                JS
             </Link>
             <div className='mobile-menu block md:hidden'>
               {!navbarOpen ? (
