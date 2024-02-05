fac15bc6c277fcc648ab7b480df2cb42ca7c7f26
juliscrep <julietascrepnik@gmail.com>
2024-02-05 17:37:03 -0300

    se agrega project section

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -1,8 +1,9 @@
 "use client";
 
-import React, { useState } from 'react';
+import React, { useState, useRef  } from 'react';
 import ProjectCard from './ProjectCard';
 import ProjectTag from './ProjectTag';
+import { motion, useInView } from "framer-motion";
 
 const projectsData = [
     {
@@ -11,8 +12,8 @@ const projectsData = [
         description:"This project consists of a website for a company called North Winds that provides services to wind parks. I used HTML, CSS, JavaScript, and Next.js. It was deployed on Vercel. Desktop and mobile version.",
         image: "/images/projects/northwinds.png",
         tag: ["All","Web"],
-        gitUrl:"/", 
-        previewUrl:"/",
+        gitUrl:"https://github.com/juliscrep/North-Winds-Project-", 
+        previewUrl:"https://northwinds.vercel.app/",
     },
     {
         id:2,
@@ -20,8 +21,8 @@ const projectsData = [
         description:"This project presents the proposed solution for the integrative project in the course 'Topics in Software Engineering II' within the Master's Degree in Software program. The proposal involves the development of a web service (API) that provides access to a neural network model.The main goal of this model is to diagnose the cardiac risk of a patient based on provided clinical data, such as cholesterol level, blood pressure, blood sugar level, age, overweight status, and smoking habit.The logical system model is based on a microservices architecture using Python and MongoDB.",
         image: "/images/projects/patientsCardiacRisk.png",
         tag: ["All","Academic"],
-        gitUrl:"/", 
-        previewUrl:"/",
+        gitUrl:"https://github.com/fueguino84/tp_final_topicos2", 
+        previewUrl:"https://github.com/fueguino84/tp_final_topicos2",
     },
     {
         id:3,
@@ -29,8 +30,8 @@ const projectsData = [
         description:"In this project, the proposed solution for the integrative project of the course 'Software Techniques and Tools' in the Master's Degree in Software program. The proposal involves the development of a blockchain using Node.js.",
         image: "/images/projects/blockchain.png",
         tag: ["All","Academic"],
-        gitUrl:"/", 
-        previewUrl:"/",
+        gitUrl:"https://github.com/cuchujero/tp_final_tyh2023", 
+        previewUrl:"https://github.com/cuchujero/tp_final_tyh2023",
     },
     {
         id:4,
@@ -38,8 +39,8 @@ const projectsData = [
         description:"In this project, I tested the 'Artic' mobile application. The application was designed to enhance the visitor experience by providing detailed information about exhibitions, event schedules, and activities that users could register for. Additionally, it allowed users to explore multimedia content related to works of art",
         image: "/images/projects/artic.png",
         tag: ["All","Testing"],
-        gitUrl:"/", 
-        previewUrl:"/",
+        gitUrl:"https://drive.google.com/drive/folders/1e2NSdVyXH28ixfqq8AwI8WlvHML9ztp8?usp=drive_link", 
+        previewUrl:"https://drive.google.com/drive/folders/1e2NSdVyXH28ixfqq8AwI8WlvHML9ztp8?usp=drive_link",
     },
 
 ]
@@ -51,12 +52,22 @@ const ProjectsSection = () => {
     setTag(newTag);
   };
 
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
   const filteredProjects = projectsData.filter((project) => 
     project.tag.includes(tag)
   );
 
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+
   return (
-    <><h2 className='text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12'>
+    <><section id="projects">
+       <h2 className='text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12'>
          My Projects
        </h2>
        <div className='text-white flex flex-row justify-center items-center gap-2 py-6'>
@@ -81,17 +92,29 @@ const ProjectsSection = () => {
             isSelected={tag === "Web"} 
            />
 
-       </div>
-    <div>{filteredProjects.map((project) => 
-       <ProjectCard 
-         key={project.id} 
-         title={project.title} 
-         description={project.description} 
-         imgUrl={project.image}
-         gitUrl={project.gitUrl}
-         previewUrl={project.previewUrl}
-        />)}</div></>
+        </div>
+        <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
+     </section></>
   )
 }
 
-export default ProjectsSection
+export default ProjectsSection;
