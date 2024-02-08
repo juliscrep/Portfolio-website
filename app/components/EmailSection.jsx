b3b1cba5e78357b5748fd1d91fe111f7ee07813a
juliscrep <julietascrepnik@gmail.com>
2024-02-08 15:43:54 -0300

    mejoras al codigo

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 const EmailSection = () => {
   return (
     <section className='grid md:grid-cols-2 my-12 md:my-12 py-4 gap-4 relative' id="contact">
-        <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
+        <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
         <div className='z-10'>
             <h5 className="text-xl font-bold text-white my-2">
                 Let&apos;s Connect
@@ -48,7 +48,7 @@ const EmailSection = () => {
                     <textarea name='message' id='message' className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
                      placeholder="Let's talk about..."/>                    
                 </div>
-                <button type='submit' className='bg-blue-500 hover:bg-blue-700 text-white font-medium py-2.5 px-5 rounded-lg w-full'>Submit</button>
+                <button type='submit' className='bg-primary-500 hover:bg-primary-700 text-white font-medium py-2.5 px-5 rounded-lg w-full'>Submit</button>
             </form>
         </div>
     </section>
--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 via-blue-500 to-blue-200">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-600 via-primary-500 to-primary-200">
               Hello, I&apos;m{" "}
             </span>
             <br></br>
@@ -40,13 +40,13 @@ const HeroSection = () => {
           <div>
             <Link
               href="/#contact"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-700 via-blue-500 to-blue-300 hover:bg-slate-200 text-white"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-700 via-primary-500 to-primary-300 hover:bg-slate-200 text-white"
             >
               Hire Me
             </Link>
             <Link
               href="/images/cv.pdf" target="_blank"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-700 via-blue-500 to-blue-200 hover:bg-slate-800 text-white mt-3"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-700 via-primary-500 to-primary-200 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
